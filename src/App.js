3ed97c9800637447ecb612b17b79c75210a21f44
jowiealegre <jowiealegre@gmail.com>
2020-01-27 17:36:47 -0300

    ejercicio 1,2 y creacion de capeta galeria con componentes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1>DATOS PERSONALES</h1>
+        <DatosPersonales/>
+        </header>
+</div>
   );
 }
+
+class DatosPersonales extends React.Component {
+  render (){
+    return (
+      <div>
+        <ul >
+          <li>Nombre: Jowie</li>
+          <br></br>
+          <li>Apeliido: Alegre</li>
+          <br></br>
+          <li>Telefono: 11-3459-0361</li>
+          <br></br>
+        </ul>
+      
+        <form>
+         <label>
+        Nombre:
+     <input type="text" name="nombre"/>
+        </label>
+        <br></br>
+<label>
+Apellido:
+<input type="text" name="nombre"/>
+</label>
+<br></br>
+<label>
+Télefono:
+<input type="text" name="nombre"/>
+</label>
+<br></br>
+<input type="submit" name="enviar"/>
+</form>
+
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
